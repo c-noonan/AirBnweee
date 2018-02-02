056634fe367daeaa28e1dcfe76ce057c8344dbd3
Katie Noonan <catherine.c.noonan@gmail.com>
2018-02-01 18:08:54 -0800

    Final touches on session/signup styling

--- frontend/components/session/modal_style.js
+++ frontend/components/session/modal_style.js
@@ -6,7 +6,15 @@ const style = {
     right             : 0,
     bottom            : 0,
     backgroundColor   : 'rgba(0, 0, 0, 0.75)',
-  }
+  },
+  content : {
+    borderRadius      : '0px',
+    top               : '50%',
+    left              : '50%',
+    transform         : 'translate(-50%, -50%)',
+    width             : '520px',
+    height            : '450px'
+  },
 };
 
 export default style;
