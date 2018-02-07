4e973cd51c0851b84b0199dda079c40a6c5e8ee1
Katie Noonan <catherine.c.noonan@gmail.com>
2018-02-06 16:17:40 -0800

    Make each index itema a link and put final touches on index styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import NavBarContainer from './nav_bar/nav_bar_container';
-// import SpotIndexContainer from './spots/spot_index_container';
 import SpotShowContainer from './spots/spot_show_container';
 import SearchContainer from './search/search_container';
 
@@ -12,5 +11,3 @@ export default () => (
     <Route exact path='/spots' component={SearchContainer} />
   </div>
 );
-
-// <Route exact path='/spots' component={SpotIndexContainer} />
--- frontend/components/spots/spot_index_item.jsx
+++ frontend/components/spots/spot_index_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SpotIndexItem extends React.Component {
 
@@ -10,25 +11,27 @@ class SpotIndexItem extends React.Component {
     console.log(this.props);
     return(
       <div>
-        <ul className='list-elements'>
-          <img src={this.props.spot.image_url} />
-          <span>
-            <li id='header'>
-              ENTIRE PLACE
-            </li>
-            <li id='title'>
-              {this.props.spot.title}
-            </li>
-            <ul id='spot-items'>
-              <li>{this.props.spot.guests} guests ·&nbsp;</li>
-              <li>{this.props.spot.bedrooms} bedrooms ·&nbsp;</li>
-              <li>{this.props.spot.bathrooms} bathrooms</li>
-            </ul>
-            <li id='price'>
-              ${this.props.spot.price} per night
-            </li>
-          </span>
-        </ul>
+        <Link to={`/spots/${this.props.spot.id}`}>
+          <ul className='list-elements'>
+            <img src={this.props.spot.image_url} />
+            <span>
+              <li id='header'>
+                ENTIRE PLACE
+              </li>
+              <li id='title'>
+                {this.props.spot.title}
+              </li>
+              <ul id='spot-items'>
+                <li>{this.props.spot.guests} guests ·&nbsp;</li>
+                <li>{this.props.spot.bedrooms} bedrooms ·&nbsp;</li>
+                <li>{this.props.spot.bathrooms} bathrooms</li>
+              </ul>
+              <li id='price'>
+                ${this.props.spot.price} per night
+              </li>
+            </span>
+          </ul>
+        </Link>
       </div>
     );
   }
