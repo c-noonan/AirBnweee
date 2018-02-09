8532ce6728553fb8c47fe80a3a88a4babfc1fb25
Katie Noonan <catherine.c.noonan@gmail.com>
2018-02-09 12:56:06 -0800

    Include figaro gem, start bookings validation, include additional seed data to filter by location

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ApplicationRecord
   validates :user_id, :spot_id, :start_date, :end_date, :guests, presence: true
 
+  validate :denied_requests
+
   belongs_to :user,
     primary_key: :id,
     foreign_key: :user_id,
@@ -10,5 +12,19 @@ class Booking < ApplicationRecord
     primary_key: :id,
     foreign_key: :spot_id,
     class_name: 'Spot'
+
+   def overlapping_requests
+     Bookings
+       .where.not(id: self.id)
+       .where(spot_id: spot_id)
+       .where.not('start_date > :end_date OR end_date < :start_date',
+                  start_date: start_date, end_date: end_date)
+   end
+
+   def denied_requests
+     unless overlapping_requests.empty?
+       errors[:base] << ['Booking conflicts with existing booking']
+     end
+   end
 
 end
