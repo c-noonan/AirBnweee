3ae2c800e732ae876020d5ac81ea48542173b9cb
Katie Noonan <catherine.c.noonan@gmail.com>
2018-03-08 19:05:43 -0800

    Style explore links

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -38,30 +38,34 @@ class SearchBar extends React.Component {
             </button>
           </form>
         </div>
-        <div>
+        <div className='explore-section'>
           <p id="explore">Explore Airbnweee</p>
           <ul className="themeparks">
             <Link to="/spots/?search=disneyland">
               <li className="disneyland">
                 <img src="https://images.unsplash.com/photo-1484923720570-4bc210954735?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=039cf9c30be0686b5287cd43e45de854&auto=format&fit=crop&w=750&q=80" />
+                <p>Disneyland</p>
               </li>
             </Link>
 
             <Link to="/spots/?search=universal">
               <li className="universal-studios">
                 <img src="https://images.unsplash.com/photo-1501256504904-1fbe305bb538?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6b765ff3866bb2594a208a58933a996d&auto=format&fit=crop&w=1350&q=80" />
+                <p>Universal Studios</p>
               </li>
             </Link>
 
             <Link to="/spots/?search=flags">
               <li className="six-flags">
                 <img src="https://images.unsplash.com/photo-1505731110654-99d7f7f8e39c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ddd2e325a9114d0996dd4ea74d89400a&auto=format&fit=crop&w=667&q=80" />
+                <p>Six Flags</p>
               </li>
             </Link>
 
             <Link to="/spots">
               <li className="explore-all">
                 <img src="https://images.unsplash.com/photo-1451732401917-66d80311800f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a26de8df7ede7138df3912152d40dc15&auto=format&fit=crop&w=800&q=80" />
+                <p>Explore All</p>
               </li>
             </Link>
           </ul>
