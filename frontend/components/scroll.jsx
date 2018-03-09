75bc680011ec66bbf46981ff1d1bdd96be966c29
Katie Noonan <catherine.c.noonan@gmail.com>
2018-03-09 13:14:01 -0800

    Fix scroll capability

---
+++ frontend/components/scroll.jsx
@@ -0,0 +1,16 @@
+import { withRouter } from 'react-router-dom';
+import React from 'react';
+
+class ScrollToTop extends React.Component {
+    componentDidUpdate(prevProps) {
+        if (this.props.location !== prevProps.location) {
+            window.scrollTo(0, 0);
+        }
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
+export default withRouter(ScrollToTop);
