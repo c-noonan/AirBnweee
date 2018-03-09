9f2d827e38a4dd71494e14b0488b72a02dab50a6
Katie Noonan <catherine.c.noonan@gmail.com>
2018-03-08 17:32:08 -0800

    Fix search bar centering

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -26,53 +26,55 @@ class SearchBar extends React.Component {
   render(){
     return (
       <div className='general-container'>
-      <div className='background'></div>
-      <div className='search-bar'>
-        <span>Airbnweee</span>
-        <p>Book homes around the world <br/>
-        near all your favorite theme parks.</p>
-        <form className='search-form' onSubmit={this.handleSubmit}>
-          <img src='https://d30y9cdsu7xlg0.cloudfront.net/png/15028-200.png'/>
-          <input
-            id="search-form-input"
-            type='text'
-            placeholder='Discover adventure...'
-            value={this.state.search}
-            onChange={this.updateForm("search")}
-            />
-          <button id='search-button' onSubmit={this.handleSubmit}>Search</button>
-        </form>
-        <p id='explore'>
-          Explore Airbnweee
-        </p>
-        <ul className='themeparks'>
+        <div className='background'></div>
+          <div className='search-bar'>
+            <span>Airbnweee</span>
+            <p>Book homes around the world <br/>
+            near all your favorite theme parks.</p>
+            <form className='search-form' onSubmit={this.handleSubmit}>
+              <img src='https://d30y9cdsu7xlg0.cloudfront.net/png/15028-200.png'/>
+              <input
+                id="search-form-input"
+                type='text'
+                placeholder='Discover adventure...'
+                value={this.state.search}
+                onChange={this.updateForm("search")}
+                />
+              <button id='search-button' onSubmit={this.handleSubmit}>Search</button>
+            </form>
+            </div>
+            <div>
+              <p id='explore'>
+                Explore Airbnweee
+              </p>
+              <ul className='themeparks'>
 
-          <Link to='/spots/?search=disneyland'>
-            <li className='disneyland'>
-              <img src='https://images.unsplash.com/photo-1484923720570-4bc210954735?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=039cf9c30be0686b5287cd43e45de854&auto=format&fit=crop&w=750&q=80'/>
-            </li>
-          </Link>
+                <Link to='/spots/?search=disneyland'>
+                  <li className='disneyland'>
+                    <img src='https://images.unsplash.com/photo-1484923720570-4bc210954735?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=039cf9c30be0686b5287cd43e45de854&auto=format&fit=crop&w=750&q=80'/>
+                  </li>
+                </Link>
 
-          <Link to='/spots/?search=universal'>
-            <li className='universal-studios'>
-              <img src='https://images.unsplash.com/photo-1501256504904-1fbe305bb538?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6b765ff3866bb2594a208a58933a996d&auto=format&fit=crop&w=1350&q=80'/>
-            </li>
-          </Link>
+                <Link to='/spots/?search=universal'>
+                  <li className='universal-studios'>
+                    <img src='https://images.unsplash.com/photo-1501256504904-1fbe305bb538?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6b765ff3866bb2594a208a58933a996d&auto=format&fit=crop&w=1350&q=80'/>
+                  </li>
+                </Link>
 
-          <Link to='/spots/?search=flags'>
-            <li className='six-flags'>
-              <img src='https://images.unsplash.com/photo-1505731110654-99d7f7f8e39c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ddd2e325a9114d0996dd4ea74d89400a&auto=format&fit=crop&w=667&q=80'/>
-            </li>
-          </Link>
+                <Link to='/spots/?search=flags'>
+                  <li className='six-flags'>
+                    <img src='https://images.unsplash.com/photo-1505731110654-99d7f7f8e39c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ddd2e325a9114d0996dd4ea74d89400a&auto=format&fit=crop&w=667&q=80'/>
+                  </li>
+                </Link>
 
-          <Link to='/spots'>
-            <li className='explore-all'>
-              <img src='https://images.unsplash.com/photo-1451732401917-66d80311800f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a26de8df7ede7138df3912152d40dc15&auto=format&fit=crop&w=800&q=80'/>
-            </li>
-          </Link>
-        </ul>
-      </div>
-      </div>
+                <Link to='/spots'>
+                  <li className='explore-all'>
+                    <img src='https://images.unsplash.com/photo-1451732401917-66d80311800f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a26de8df7ede7138df3912152d40dc15&auto=format&fit=crop&w=800&q=80'/>
+                  </li>
+                </Link>
+              </ul>
+            </div>
+        </div>
     );
   }
 
